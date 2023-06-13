c967aa89b9c4e19ace66dd77ebc92438bae5997c
Christian Zamiela <cez39@msstate.edu>
2023-06-13 18:35:29 -0500

    Update build_ws

--- build_ws.sh
+++ build_ws.sh
@@ -4,3 +4,6 @@ cd ..
 mkdir WA-DED-ROS2-ws
 cd WA-DED-ROS2-ws
 mkdir src
+cd src
+git clone https://github.com/evocortex/optris_drivers2.git
+git clone https://github.com/PickNikRobotics/abb_ros2.git
